0a7c7b7e37e7d00cf0e925cb216f5ee587aec1c5
andrew cunningham <andrewjasoncunningham@yahoo.com>
2019-05-29 16:17:53 -0700

    completed per instructions

--- server.js
+++ server.js
@@ -6,9 +6,10 @@ const port = process.env.PORT || 8000;
 let storage = [];
 app.use(bodyParser.json());
 
+//add new user
 app.post('/storage/:index', function(req, res) {
   var index = Number.parseInt(req.params.index);
- console.log(req.body)
+ 
   if (Number.isNaN(index) || index < 0 ) {
     
     return res.sendStatus(404);
@@ -19,13 +20,58 @@ app.post('/storage/:index', function(req, res) {
   if (!newUser) {
     return res.sendStatus(400);
   }
- console.log(req)
+ //console.log(req)
   storage[index] = newUser;
  
-  console.log(storage)
+  //console.log(storage)
   res.json(newUser);
 });
+
+//Get all users
+app.get('/storage', function(req, res) {
+  res.json(storage);
+});
+
+//Get User by name
+app.get('/storage/:name', function(req, res) {
+  let names=req.params.name
+  let userByName={}
+  for(let i=0; i< storage.length; i++){
+   // console.log(storage[i]['name'])
+    if(storage[i]['name'] == names){
+      userByName=storage[i]
+    }
+  }
+  res.json(userByName);
+});
+
+app.put('/storage/:name', function(req, res) {
+  let names=req.params.name
+  let userByNameUpdate={}
+  for(let i=0; i< storage.length; i++){
+   // console.log(storage[i]['name'])
+    if(storage[i]['name'] == names){
+      userByNameUpdate=req.body
+      storage[i]=req.body
+    }
+  }
+  res.json(userByNameUpdate);
+});
+app.delete('/storage/:name', function(req, res) {
+  let names=req.params.name
+  for(let i=0; i< storage.length; i++){
+    // console.log(storage[i]['name'])
+     if(storage[i]['name'] == names){
+       
+       storage.splice([i],1)
+     }
+   }
+   console.log(storage)
+   res.json(storage)
+  });
 
 app.listen(port, ()=>{
   console.log(`Listening on port ${port}`);
 })
+
+
